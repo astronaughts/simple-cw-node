2e0dd8f02965b154a313ab47c02a21f6159ba6a4
astronaughts <hello@astronaughts.net>
2018-05-24 18:00:47 +0900

    Remove grunt.

--- boostrap.js
+++
@@ -1,9 +0,0 @@
-var cw = require('./lib/chatwork')();
-
-cw.init({ token: process.env.CHATWORK_API_TOKEN });
-
-cw.get('me').done(function (res) {
-    console.log(res.body);
-}).fail(function (err) {
-    console.log(err)
-});
